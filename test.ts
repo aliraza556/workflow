54901361f7c078c3e272a0ff83383ced199d6e23
Ali Raza <87068339+aliraza556@users.noreply.github.com>
2023-12-26 10:35:40 -0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,13 @@
-Hello world
+function addTwoDigits(a: number, b: number): number {
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {
+        throw new Error('Both arguments must be integers.');
+    }
+    if (a < 0 || a > 9 || b < 0 || b > 9) {
+        throw new Error('Both arguments must be single digits (0-9).');
+    }
+    return a + b;
+}
+
+// Example usage
+const result = addTwoDigits(5, 3);
+console.log(result); // Output will be 8
